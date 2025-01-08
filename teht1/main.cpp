28cf727d2a383c186782f5b8895087ef21f170f2
harkonennen <harri.antti.harkonen@gmail.com>
2025-01-08 13:42:02 +0200

    seconds commit

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
+int game(int);
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int scaleNumber;
+    cout << "give number" << endl;
+    cin >> scaleNumber;
+    int tries = game(scaleNumber);
+    cout << "number of tries:" << tries << endl;
     return 0;
+}
+
+int game(int maxnum){
+    srand(time(0));
+    int randomNum = rand()%maxnum+1;
+    int userNum;
+    int guessAmount =0;
+
+    while(randomNum != userNum){
+        cout << "guess a number between 1 -" << maxnum << endl;
+        cin >> userNum;
+        guessAmount++;
+
+        if (randomNum == userNum){
+            cout << "equal to random number" << endl;
+            cout << "you win" << endl;
+        } else if (randomNum > userNum){
+            cout << "smaller than random number, try again" << endl;
+        } else {
+            cout << "bigger than randon number, try again" << endl;
+        }
+    }
+    return guessAmount;
 }
